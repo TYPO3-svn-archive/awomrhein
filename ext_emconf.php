263a8306b4f6ea00dcde96d47847944083f3b13a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-28 16:33:20 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/awomrhein/trunk@79196 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "orgesab".
- *
- * Auto generated 16-04-2013 10:55
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'AWO Mittelrhein',
 	'description' => 'AWO Mittelrhein',
